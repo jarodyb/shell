190f58498265b50e6b31437e4886bf54a25937e7
jarodyb <77947306+jarodyb@users.noreply.github.com>
2021-02-27 16:52:04 +0800

    Add files via upload

---
+++ log-analysis.sh
@@ -0,0 +1,43 @@
+#!/bin/sh
+[ -f /etc/init.d/functions ] && . /etc/init.d/functions
+
+
+logpath(){
+
+read -p "Please input the path of logs(/tmp/logs/):" spath
+
+ pathcheck
+}
+
+
+pathcheck(){
+
+ if [ ! -d $spath ]; then
+  echo "This path does not exist!" 
+  logpath
+else
+  path=$(find $spath|tail -n +2)
+  key
+ fi
+}
+key(){
+read -p "Please input your keyword:" keyword
+
+mkdir ./${keyword}
+
+for file in $path ;
+do
+ eval sed -n '/$keyword/p' ${file} >> ./${keyword}/${keyword}_log.log
+done
+
+
+cat ./${keyword}/${keyword}_log.log |tr -s ' '|cut -d' ' -f1| sort -r| uniq -c| sort -k1 -nr >>./${keyword}/${keyword}_ip.txt
+
+ip=$(awk '{print $2}' ./${keyword}/${keyword}_ip.txt)
+
+for i in $ip
+ do  
+eval sed -n '/${i}/p' ${file} >> ./${keyword}/${keyword}_ip_log.log
+done
+}
+logpath
